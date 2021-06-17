ce1d6eb0956dc6584e60164f4077503ba9a52ce5
Pierre Silva <pierremichelsilva@gmail.com>
2021-06-17 14:02:14 -0500

    Some data

--- routes/api.php
+++ routes/api.php
@@ -120,7 +120,7 @@ Route::get('test/availabilities', function(Request $request) {
                 SELECT reserva.numres, reserva.numhab, reserva.estado, habitacion.codcla
                 FROM `reserva`
                 INNER JOIN habitacion ON reserva.numhab = habitacion.numhab
-                WHERE reserva.feclle >= '{$request->get('start')}' AND reserva.fecsal <= '{$request->get('end')}'
+                WHERE reserva.feclle <= '{$request->get('end')}' AND reserva.fecsal >= '{$request->get('start')}'
                 AND reserva.estado IN ('P','G')
                 AND habitacion.codcla = {$request->get('class')}
                 AND habitacion.tipo = 'V'
