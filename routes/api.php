e46ee5e2d77858541a9780b990ed9f37f41959e7
Pierre Silva <pierremichelsilva@gmail.com>
2021-06-17 15:05:22 -0500

    Some data

--- routes/api.php
+++ routes/api.php
@@ -137,7 +137,7 @@ Route::get('test/availabilities', function(Request $request) {
                 INNER JOIN habitacion ON reserva.numhab = habitacion.numhab
                 INNER JOIN folio ON reserva.numhab = folio.numres
                 WHERE reserva.feclle >= '{$request->get('start')}' AND reserva.fecsal <= '{$request->get('end')}'
-                AND reserva.estado IN ('H')
+                AND reserva.estado IN ('P','G','H')
                 AND folio.estado IN ('I', 'O')
                 AND habitacion.codcla = {$request->get('class')}
                 AND habitacion.tipo = 'V'
