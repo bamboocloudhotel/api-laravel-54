4ad3f4c2b5f20d2acb817bbfbb568ee70c36d56d
Full Stack <fullstack@nextgy.co>
2021-08-10 19:27:43 -0500

    fix availability

--- routes/api.php
+++ routes/api.php
@@ -1,39 +1,40 @@
 <?php
 
-use Illuminate\Http\Request;
+  use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-use App\Models\Dathot;
-use App\Models\CrGuarantee;
+  /*
+  |--------------------------------------------------------------------------
+  | API Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register API routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | is assigned the "api" middleware group. Enjoy building your API!
+  |
+  */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+  use App\Models\Dathot;
+  use App\Models\CrGuarantee;
+
+  Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+  });
 
-Route::any('test', function(Request $request) {
-	return response()->json([
-        'message' => 'OK',
-        'data' => $request->all()
+  Route::any('test', function (Request $request) {
+    return response()->json([
+      'message' => 'OK',
+      'data' => $request->all()
     ], 200);
-});
+  });
 
-Route::post('view-guarantee', function (Request $request) {
-	
-	$xmlController = new \App\Http\Controllers\XMLController();
+  Route::post('view-guarantee', function (Request $request) {
+
+    $xmlController = new \App\Http\Controllers\XMLController();
 
     if (!$request->get('hotelCode')) {
-        return response()->json([
-            'message' => 'Debe enviar el código del hotel!'
-        ], 404);
+      return response()->json([
+        'message' => 'Debe enviar el código del hotel!'
+      ], 404);
     }
 
     $xmlController->getInstance($request->get('hotelCode'));
@@ -41,9 +42,9 @@ Route::post('view-guarantee', function (Request $request) {
     $xmlController = new \App\Http\Controllers\XMLController();
 
     if (!$request->get('hotelCode')) {
-        return response()->json([
-            'message' => 'Debe enviar el código del hotel!'
-        ], 404);
+      return response()->json([
+        'message' => 'Debe enviar el código del hotel!'
+      ], 404);
     }
 
     $xmlController->getInstance($request->get('hotelCode'));
@@ -52,86 +53,90 @@ Route::post('view-guarantee', function (Request $request) {
 
     if ($dathot->guarantee_password == $request->get('guarantee_password')) {
 
-        $guarantee = CrGuarantee::where('numres', $request->get('numres'))->first();
+      $guarantee = CrGuarantee::where('numres', $request->get('numres'))->first();
 
-        if (!$guarantee) {
-            return response()->json([
-                'message' => 'Reserva no valida'
-            ], 422);
-        }
-
+      if (!$guarantee) {
         return response()->json([
-            'message' => 'Garantia obtenida con éxito!',
-            'data' => $guarantee->toArray()
-        ], 200);
+          'message' => 'Reserva no valida'
+        ], 422);
+      }
+
+      return response()->json([
+        'message' => 'Garantia obtenida con éxito!',
+        'data' => $guarantee->toArray()
+      ], 200);
     }
 
     return response()->json([
-        'message' => 'Clave no valida'
+      'message' => 'Clave no valida'
     ], 422);
-});
+  });
 
-Route::any('rgbridgeapi/push/receive', 'XMLController@index');
+  Route::any('rgbridgeapi/push/receive', 'XMLController@index');
 
-Route::get('radius/users', 'Api\FreeRadiusController@getUsers');
-Route::get('radius/users/{username}', 'Api\FreeRadiusController@getUser');
-Route::post('radius/users', 'Api\FreeRadiusController@addUser');
-Route::post('radius/users/{username}/delete', 'Api\FreeRadiusController@removeUser');
+  Route::get('radius/users', 'Api\FreeRadiusController@getUsers');
+  Route::get('radius/users/{username}', 'Api\FreeRadiusController@getUser');
+  Route::post('radius/users', 'Api\FreeRadiusController@addUser');
+  Route::post('radius/users/{username}/delete', 'Api\FreeRadiusController@removeUser');
 
-Route::get('databases', 'Api\DatabasesController@index');
-Route::post('databases', 'Api\DatabasesController@store');
-Route::put('databases/{id}', 'Api\DatabasesController@update');
+  Route::get('databases', 'Api\DatabasesController@index');
+  Route::post('databases', 'Api\DatabasesController@store');
+  Route::put('databases/{id}', 'Api\DatabasesController@update');
 
-Route::get('soap/test', 'SoapTestController@show');
-Route::get('soap/tests', 'SoapTestController@testing');
+  Route::get('soap/test', 'SoapTestController@show');
+  Route::get('soap/tests', 'SoapTestController@testing');
 
-Route::get('soap/cr-reservas/hotels', 'Api\TestSoapController@getHotels');
-Route::get('soap/cr-reservas/rates/{hotelId?}', 'Api\TestSoapController@getRates');
-Route::get('soap/cr-reservas/rooms/{hotelId?}', 'Api\TestSoapController@getRooms');
-Route::get('soap/cr-reservas/portals/{hotelId?}', 'Api\TestSoapController@getPortals');
-Route::get('soap/cr-reservas/reservations/{startDate?}/{endDate?}/{hotelId?}/{dlm?}', 'Api\TestSoapController@getReservations');
-Route::get('soap/cr-reservas/availability/{startDate?}/{endDate?}/{hotelId?}/{clahab?}', 'Api\TestSoapController@getAvailability');
-Route::get('soap/cr-reservas/modify-inventory/{startDate}/{endDate}/{roomTypeId}/{oldStartDate?}/{oldEndDate?}', 'Api\TestSoapController@modifyInventoryByDatesAndRoom');
-Route::get('soap/cr-reservas/modify-inventory', 'Api\TestSoapController@modifyInventory');
+  Route::get('soap/cr-reservas/hotels', 'Api\TestSoapController@getHotels');
+  Route::get('soap/cr-reservas/rates/{hotelId?}', 'Api\TestSoapController@getRates');
+  Route::get('soap/cr-reservas/rooms/{hotelId?}', 'Api\TestSoapController@getRooms');
+  Route::get('soap/cr-reservas/portals/{hotelId?}', 'Api\TestSoapController@getPortals');
+  Route::get('soap/cr-reservas/reservations/{startDate?}/{endDate?}/{hotelId?}/{dlm?}', 'Api\TestSoapController@getReservations');
+  Route::get('soap/cr-reservas/availability/{startDate?}/{endDate?}/{hotelId?}/{clahab?}', 'Api\TestSoapController@getAvailability');
+  Route::get('soap/cr-reservas/modify-inventory/{startDate}/{endDate}/{roomTypeId}/{oldStartDate?}/{oldEndDate?}', 'Api\TestSoapController@modifyInventoryByDatesAndRoom');
+  Route::get('soap/cr-reservas/modify-inventory', 'Api\TestSoapController@modifyInventory');
 
-Route::get('soap/bamboo/availability/{startDate?}/{endDate?}/{hotelId?}', 'Api\TestSoapController@getBambooQuantityAvailability');
+  Route::get('soap/bamboo/availability/{startDate?}/{endDate?}/{hotelId?}', 'Api\TestSoapController@getBambooQuantityAvailability');
 
-Route::get('test/availabilities', function(Request $request) {
+  Route::get('test/availabilities', function (Request $request) {
 
-  $testSoapController = new \App\Http\Controllers\Api\TestSoapController($request);
+    $testSoapController = new \App\Http\Controllers\Api\TestSoapController($request);
 
-  $testSoapController->setRateGainConfig($request->get('hotelId'));
+    $testSoapController->setRateGainConfig($request->get('hotelId'));
 
-  $roomsBlocked = collect(\DB::connection('on_the_fly')->select("
-                SELECT blohab.numhab
-                FROM blohab
-                LEFT JOIN habitacion ON blohab.numhab = habitacion.numhab
-                WHERE blohab.fecini <= '{$request->get('end')}' AND blohab.fecfin >= '{$request->get('start')}'
-                AND blohab.fecdes IS NULL
-                AND habitacion.codcla = {$request->get('class')}
-                AND habitacion.tipo = 'V'
-            "));
+    $sql = "
+    SELECT blohab.numhab
+    FROM blohab
+    LEFT JOIN habitacion ON blohab.numhab = habitacion.numhab
+    WHERE blohab.fecini <= '{$request->get('end')}' AND blohab.fecfin >= '{$request->get('start')}'
+    AND blohab.fecdes IS NULL
+    AND habitacion.codcla = {$request->get('class')}
+    AND habitacion.tipo = 'V'
+    ";
 
-  foreach ($roomsBlocked as $roomBlocked) {
-    $roomsOccupied[] = $roomBlocked->numhab;
-  }
+    $roomsBlocked = collect(\DB::connection('on_the_fly')->select($sql));
 
-  $roomsReserved = collect(\DB::connection('on_the_fly')->select("
-                SELECT reserva.numres, reserva.numhab, reserva.estado, habitacion.codcla
-                FROM `reserva`
-                LEFT JOIN habitacion ON reserva.numhab = habitacion.numhab
-                WHERE reserva.feclle <= '{$request->get('start')}' AND reserva.fecsal >= '{$request->get('end')}'
-                AND reserva.estado IN ('P','G')
-                AND habitacion.codcla = {$request->get('class')}
-                AND habitacion.tipo = 'V'
-                ORDER BY reserva.numhab ASC
-            "));
+    foreach ($roomsBlocked as $roomBlocked) {
+      $roomsOccupied[] = $roomBlocked->numhab;
+    }
 
-  foreach ($roomsReserved as $roomReserved) {
-    $roomsOccupied[] = $roomReserved->numhab;
-  }
+    $sql = "
+    SELECT reserva.numres, reserva.numhab, reserva.estado, habitacion.codcla
+    FROM `reserva`
+    LEFT JOIN habitacion ON reserva.numhab = habitacion.numhab
+    WHERE reserva.feclle <= '{$request->get('start')}' AND reserva.fecsal >= '{$request->get('end')}'
+    AND reserva.estado IN ('P','G')
+    AND habitacion.codcla = {$request->get('class')}
+    AND habitacion.tipo = 'V'
+    ORDER BY reserva.numhab ASC
+    ";
 
-  $sql = "
+    $roomsReserved = collect(\DB::connection('on_the_fly')->select($sql));
+
+    foreach ($roomsReserved as $roomReserved) {
+      $roomsOccupied[] = $roomReserved->numhab;
+    }
+
+    $sql = "
     SELECT reserva.numres, reserva.numhab, reserva.estado, habitacion.codcla, folio.numfol, folio.estado
     FROM `reserva`
     INNER JOIN habitacion ON reserva.numhab = habitacion.numhab
@@ -141,36 +146,38 @@ Route::get('test/availabilities', function(Request $request) {
     AND folio.estado IN ('I')
     AND habitacion.codcla = {$request->get('class')}
     AND habitacion.tipo = 'V'
-  ";
+    ";
 
-  $roomsHosted = collect(\DB::connection('on_the_fly')->select($sql));
+    $roomsHosted = collect(\DB::connection('on_the_fly')->select($sql));
 
-  foreach ($roomsHosted as $roomHosted) {
-    $roomsOccupied[] = $roomHosted->numhab;
-  }
+    foreach ($roomsHosted as $roomHosted) {
+      $roomsOccupied[] = $roomHosted->numhab;
+    }
 
-  $roomsOccupied = collect($roomsOccupied);
+    $roomsOccupied = collect($roomsOccupied);
 
-  $roomsOccupied = implode('\',\'', $roomsOccupied->sort()->unique()->values()->all());
+    $roomsOccupied = implode('\',\'', $roomsOccupied->sort()->unique()->values()->all());
 
-  $numhab = collect(\DB::connection('on_the_fly')->select("
+    $sql = "
     select habitacion.numhab 
     from habitacion 
     where habitacion.numhab not in ('{$roomsOccupied}')
     AND habitacion.codcla = {$request->get('class')}
     AND habitacion.tipo = 'V'
-  "));
+    ";
 
-  $roomsAvailable = implode('\',\'', $numhab->pluck('numhab')->unique()->sort()->values()->all());
+    $numhab = collect(\DB::connection('on_the_fly')->select($sql));
 
-  // dd($roomsBlocked, $roomsReserved, $roomsHosted, $roomsOccupied, $roomsAvailable);
+    $roomsAvailable = implode('\',\'', $numhab->pluck('numhab')->unique()->sort()->values()->all());
 
-  return response()->json([
-    'message' => "OK",
-    'available' => $roomsAvailable,
-    'notAvailable' => $roomsOccupied
-  ]);
+    // dd($roomsBlocked, $roomsReserved, $roomsHosted, $roomsOccupied, $roomsAvailable);
 
-});
+    return response()->json([
+      'message' => "OK",
+      'available' => $roomsAvailable,
+      'notAvailable' => $roomsOccupied
+    ]);
+
+  });
 
 
