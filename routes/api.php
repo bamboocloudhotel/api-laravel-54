d97e75278717289b820678d14fc4284d09fb20b9
Full Stack <fullstack@nextgy.co>
2021-08-10 19:31:41 -0500

    fix availability

--- routes/api.php
+++ routes/api.php
@@ -174,8 +174,8 @@
 
     return response()->json([
       'message' => "OK",
-      'available' => $roomsAvailable,
-      'notAvailable' => $roomsOccupied
+      'available' => $roomsAvailable == '\'\'' ? null : $roomsAvailable,
+      'notAvailable' => $roomsOccupied == '\'\'' ? null : $roomsOccupied
     ]);
 
   });
