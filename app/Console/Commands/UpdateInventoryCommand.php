f0bdc39c7a6d29e5bbe9cbd5563df2c884b523a0
Pierre Silva <pierresilva@Pierres-Mac-mini.local>
2022-02-08 18:40:05 -0500

    Update

--- app/Console/Commands/UpdateInventoryCommand.php
+++ app/Console/Commands/UpdateInventoryCommand.php
@@ -46,6 +46,9 @@ class UpdateInventoryCommand extends Command
         $instances = BambooInstance::with('bambooInstanceRooms')->get();
 
         foreach ($instances as $instance) {
+            if ($instance->rg_hotel_code  !== 'bhparque93') {
+                continue;
+            }
             $d1 = new \DateTime();
 
             echo "START " . $instance->rg_hotel_code . "\n";
