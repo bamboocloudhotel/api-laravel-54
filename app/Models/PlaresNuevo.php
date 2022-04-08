f8ec60fe0ae352f991e81537cc88ae203c3e21d8
Pierre Silva <pierremichelsilva@hotmail.com>
2022-04-08 11:58:34 -0500

    New plares reserva

---
+++ app/Models/PlaresNuevo.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Plares
+ * 
+ * @property int $numres
+ * @property int $numpla
+ * @property int $codpla
+ * @property Carbon $fecini
+ * @property Carbon $fecfin
+ * @property int $pordes
+ * @property string $tipdes
+ * @property float $subsidio
+ * @property int $valor
+ * @property int $valornoche
+ *
+ * @package App\Models
+ */
+class PlaresNuevo extends Model
+{
+    protected $connection = 'on_the_fly';
+	protected $table = 'plares_nuevos';
+	public $incrementing = false;
+	public $timestamps = false;
+
+	protected $casts = [
+		'numres' => 'int',
+		'numpla' => 'int',
+		'codpla' => 'int',
+		'pordes' => 'int',
+		'subsidio' => 'float',
+		'valor' => 'int',
+		'valornoche' => 'int'
+	];
+
+	protected $dates = [
+		'fecini',
+		'fecfin'
+	];
+
+	protected $fillable = [
+	    'numres',
+        'numpla',
+        'codpla',
+		'fecini',
+		'fecfin',
+		'pordes',
+		'tipdes',
+		'subsidio',
+		'valor',
+		'valornoche',
+        'codigocr'
+	];
+}
